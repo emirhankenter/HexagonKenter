a2a9848576e8aeb92ddb0136a45efe856fdbaa6a
Emirhan Kenter <emirhankenter@gmail.com>
2020-09-19 19:36:26 +0300

    player input control improvement

--- Assets/Game/Scripts/Behaviours/PlayerBehaviour.cs
+++ Assets/Game/Scripts/Behaviours/PlayerBehaviour.cs
@@ -14,39 +14,68 @@ namespace Game.Scripts.Behaviours
 
         public InputActions InputActions;
 
+        private HexagonBehaviour _currentHexagon;
+
         private void Awake()
         {
             InputActions = new InputActions();
             InputActions.Enable();
-            InputActions.Player.Fire.performed += OnFirePerformed;
-            InputActions.Player.Fire.canceled += OnFireCanceled;
+            InputActions.Player.Tap.performed += OnTapPerformed;
+            InputActions.Player.Tap.canceled += OnTapCanceled;
         }
 
         private void OnDestroy()
         {
             InputActions.Disable();
-            InputActions.Player.Fire.performed -= OnFirePerformed;
-            InputActions.Player.Fire.canceled -= OnFireCanceled;
+            InputActions.Player.Tap.performed -= OnTapPerformed;
+            InputActions.Player.Tap.canceled -= OnTapCanceled;
+        }
+
+        private void OnTapPerformed(InputAction.CallbackContext obj)
+        {
+            InputActions.Player.Fire.performed += OnFirePerformed;
+            InputActions.Player.Fire.canceled += OnFireCanceled;
+
+            if (GameController.Instance.CurrentLevel.TileMap.TryGetHexagonAtPoint(GameController.Instance.CameraController.GetMouseWorldPosition(), out HexagonBehaviour hexagon))
+            {
+                Deselect();
+                Select(hexagon);
+            }
+        }
+
+        private void OnTapCanceled(InputAction.CallbackContext obj)
+        {
         }
 
         private void OnFirePerformed(InputAction.CallbackContext obj)
         {
             InputActions.Player.Move.performed += OnMovePerformed;
-
-            if (GameController.Instance.CurrentLevel.TileMap.TryGetHexagonAtPoint(GameController.Instance.CameraController.GetMouseWorldPosition(), out HexagonBehaviour hexagon))
-            {
-                Debug.Log($"{hexagon.name}");
-            }
         }
 
         private void OnFireCanceled(InputAction.CallbackContext obj)
         {
             InputActions.Player.Move.performed -= OnMovePerformed;
+            InputActions.Player.Fire.performed -= OnFireCanceled;
         }
 
         private void OnMovePerformed(InputAction.CallbackContext obj)
         {
             var position = obj.ReadValue<Vector2>();
+        }
+
+        private void Select(HexagonBehaviour hexagon)
+        {
+            _currentHexagon = hexagon;
+            _currentHexagon.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+
+        private void Deselect()
+        {
+            if (_currentHexagon != null)
+            {
+                _currentHexagon.GetComponent<SpriteRenderer>().color = Color.white;
+                _currentHexagon = null;
+            }
         }
     }
 }
--- Assets/Game/Scripts/Models/InputActions.cs
+++ Assets/Game/Scripts/Models/InputActions.cs
@@ -21,12 +21,20 @@ namespace Game.Scripts.Models
             ""id"": ""a904d303-9ccf-4fbb-8ee8-f9def4fa6f71"",
             ""actions"": [
                 {
-                    ""name"": ""Fire"",
+                    ""name"": ""Tap"",
                     ""type"": ""Button"",
                     ""id"": ""5c148fb2-c74a-4feb-81cf-dcf0d0816490"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
-                    ""interactions"": """"
+                    ""interactions"": ""Tap""
+                },
+                {
+                    ""name"": ""Fire"",
+                    ""type"": ""Button"",
+                    ""id"": ""385188da-23bf-41c6-8a61-d9091537ac8c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
                 },
                 {
                     ""name"": ""Move"",
@@ -40,23 +48,34 @@ namespace Game.Scripts.Models
             ""bindings"": [
                 {
                     ""name"": """",
-                    ""id"": ""2d2f31cd-9b45-46ab-9bc8-56de4d6a4718"",
-                    ""path"": ""<Mouse>/leftButton"",
-                    ""interactions"": ""Press"",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
                     ""id"": ""af136eb5-f162-47a8-8ca7-4b5d0cba25ae"",
                     ""path"": ""<Mouse>/position"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2d2f31cd-9b45-46ab-9bc8-56de4d6a4718"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Tap"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""41717ee5-4ae0-4204-a186-98a7e6a7bdcf"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Fire"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -67,6 +86,7 @@ namespace Game.Scripts.Models
 }");
             // Player
             m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+            m_Player_Tap = m_Player.FindAction("Tap", throwIfNotFound: true);
             m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
             m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         }
@@ -118,12 +138,14 @@ namespace Game.Scripts.Models
         // Player
         private readonly InputActionMap m_Player;
         private IPlayerActions m_PlayerActionsCallbackInterface;
+        private readonly InputAction m_Player_Tap;
         private readonly InputAction m_Player_Fire;
         private readonly InputAction m_Player_Move;
         public struct PlayerActions
         {
             private @InputActions m_Wrapper;
             public PlayerActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Tap => m_Wrapper.m_Player_Tap;
             public InputAction @Fire => m_Wrapper.m_Player_Fire;
             public InputAction @Move => m_Wrapper.m_Player_Move;
             public InputActionMap Get() { return m_Wrapper.m_Player; }
@@ -135,6 +157,9 @@ namespace Game.Scripts.Models
             {
                 if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
                 {
+                    @Tap.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnTap;
+                    @Tap.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnTap;
+                    @Tap.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnTap;
                     @Fire.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
                     @Fire.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
                     @Fire.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
@@ -145,6 +170,9 @@ namespace Game.Scripts.Models
                 m_Wrapper.m_PlayerActionsCallbackInterface = instance;
                 if (instance != null)
                 {
+                    @Tap.started += instance.OnTap;
+                    @Tap.performed += instance.OnTap;
+                    @Tap.canceled += instance.OnTap;
                     @Fire.started += instance.OnFire;
                     @Fire.performed += instance.OnFire;
                     @Fire.canceled += instance.OnFire;
@@ -157,6 +185,7 @@ namespace Game.Scripts.Models
         public PlayerActions @Player => new PlayerActions(this);
         public interface IPlayerActions
         {
+            void OnTap(InputAction.CallbackContext context);
             void OnFire(InputAction.CallbackContext context);
             void OnMove(InputAction.CallbackContext context);
         }
